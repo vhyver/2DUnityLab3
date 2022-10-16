3b63b56a70191ccdfe9f86cba384059f849edb70
Vadym Verba <71729845+vhyver@users.noreply.github.com>
2022-10-16 19:31:52 +0300

    Setting up animations + dashing

---
+++ My project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movePower = 10f;
+    public float jumpPower = 15f;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    Vector3 movement;
+    private int direction = 1;
+    bool isJumping = false;
+    private bool alive = true;
+
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 20f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+
+    [SerializeField] private TrailRenderer tr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Restart();
+        if (alive)
+        {
+            if (isDashing)
+            {
+                return;
+            }
+            Jump();
+            Run();
+            if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+            {
+                StartCoroutine(Dash());
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        anim.SetBool("isJump", false);
+    }
+
+
+    void Run()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+        anim.SetBool("isRun", false);
+
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            direction = -1;
+            moveVelocity = Vector3.left;
+
+            transform.localScale = new Vector3(direction, 1, 1);
+            if (!anim.GetBool("isJump"))
+                anim.SetBool("isRun", true);
+
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            direction = 1;
+            moveVelocity = Vector3.right;
+
+            transform.localScale = new Vector3(direction, 1, 1);
+            if (!anim.GetBool("isJump"))
+                anim.SetBool("isRun", true);
+
+        }
+        transform.position += moveVelocity * movePower * Time.deltaTime;
+    }
+    void Jump()
+    {
+        if ((Input.GetButtonDown("Jump") || Input.GetAxisRaw("Vertical") > 0)
+        && !anim.GetBool("isJump"))
+        {
+            isJumping = true;
+            anim.SetBool("isJump", true);
+        }
+        if (!isJumping)
+        {
+            return;
+        }
+
+        rb.velocity = Vector2.zero;
+
+        Vector2 jumpVelocity = new Vector2(0, jumpPower);
+        rb.AddForce(jumpVelocity, ForceMode2D.Impulse);
+
+        isJumping = false;
+    }
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+    }
+    void Restart()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            anim.SetTrigger("idle");
+            alive = true;
+        }
+    }
+}
